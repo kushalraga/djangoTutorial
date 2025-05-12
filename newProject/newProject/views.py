9d635ac4ea223e8355ab0a16b11bd4e06d2111ba
kushalraga <kushalraga20@gmail.com>
2025-05-12 16:52:56 +0530

    views and static folder set up

--- newProject/newProject/views.py
+++ newProject/newProject/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello - You are on the Home Page')
+    # return HttpResponse('Hello - You are on the Home Page')
+    return render(request, 'website/home.html')
 
 def about(request):
-    return HttpResponse('Hello - You are on the About Page')
+    # return HttpResponse('Hello - You are on the About Page')
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse('Hello - You are on the Contact Page')
+    # return HttpResponse('Hello - You are on the Contact Page')
+    return render(request, 'website/contact.html')
